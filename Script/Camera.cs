3ddb779f71915dde4df8161b9294a5bc588630aa
khaledyasser95 <khaledyasser95@gmail.com>
2017-09-08 01:14:14 +0200

    First Commit

---
+++ Script/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    private Transform pumpkinpos;
+    private float zDistance = 10f;
+    private float yDistance = 4f;
+    // Use this for initialization
+    void Awake () {
+        pumpkinpos = GameObject.Find("pumpkin").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 temp = transform.position;
+        temp.y = pumpkinpos.position.y + yDistance;
+        temp.z = pumpkinpos.position.z - zDistance;
+        transform.position = temp;
+    }
+    
+}
---
+++ Script/Pumpkin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour {
+    public float moveforce = 10f;
+    private Rigidbody mybody;
+    void Awake()
+    {
+        mybody = GetComponent<Rigidbody>();
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10)
+        {
+            Time.timeScale = 0f;
+        }
+	}
+    void FixedUpdate()
+    {
+        movepumpkin();
+    }
+    void movepumpkin()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v= Input.GetAxis("Vertical");
+        mybody.AddForce(new Vector3(h*moveforce,0f,v*moveforce));
+
+    }
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Golem" || target.tag == "Gates")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
